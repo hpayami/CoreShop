3cd23893400e1612b1f8d953131ad883d120c59d
behnam <behnam@DESKTOP-TBK0VRS>
2021-07-23 12:06:06 +0430

    add Migration initial

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,21 +18,20 @@ namespace API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            services.AddDbContext<StoreContext>(options =>{
+                options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
         }
 
